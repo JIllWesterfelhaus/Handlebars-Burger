43e93d6c7c2f247cacef372d8b2b08fb85b7e151
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-05-04 13:32:52 -0500

    updated connection.js and orm.js

--- config/connection.js
+++ config/connection.js
@@ -2,29 +2,18 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "mySQL1234!",
-  database: "burgers_DB"
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
   console.log("connected as id " + connection.threadId);
-  afterConnection();
 });
 
-function afterConnection() {
-  connection.query("SELECT * FROM burgers", function(err, res) {
-    if (err) throw err;
-    console.log(res);
-    connection.end();
-  });
-}
- ///module export?
+module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,44 @@
-//selectAll()
-//insertOne()
-//updateOne()
+//from ORM Example, Exercise 12
+var connection = require("./connection.js");
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+
+var orm = {
+  selectAll: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  insertOne: function(whatToSelect, table, orderCol) {
+      //update query string to reflect insertOne
+    var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
+    console.log(queryString);
+    connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+    var queryString =
+    //update query string to reflect updateOne
+      "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
+
+    connection.query(
+      queryString,
+      //update
+      [tableOneCol, tableOneCol, tableOne, tableOne, tableOneCol],
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
+  }
+};
+
+module.exports = orm;
+
