204dbbb83206edf884034c0e65ddc1684b141d5a
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-05-06 12:47:20 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,23 +3,12 @@ var mysql = require("mysql");
 
 //connection for mysql
 var connection = mysql.createConnection({
-  //host: "localhost",
-  //port: 3306,
-  //user: "root",
-  //password: "mySQL1234!",
-  //database: "burgers_db"
-//});
-//make connection, throw error if needed
-if (process.env.JAWSDB_URL)  {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "mySQL1234!",
-    database: "burgers_db"
-  })
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "mySQL1234!",
+  database: "burgers_db"
+});
 
 connection.connect(function(err) {
   if (err) {
@@ -28,15 +17,5 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
-
-
-
-}
-
-
-
-
-
 //export connection
 module.exports = connection;
