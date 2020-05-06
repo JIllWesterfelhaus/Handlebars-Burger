68b7bb9d4cae073a207babf04dd4179d456a2f6e
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-05-06 18:17:44 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 //require mysql
 var mysql = require("mysql");
-
-//connection for mysql
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
